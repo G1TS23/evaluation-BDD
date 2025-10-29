5b2264abfbe53e0433994e9d475f05f344858439
olivierfalahi <olivier.falahi@gmail.com>
2025-10-29 15:59:31 +0100

    Add CRUD functionality for comments: implement `create`, `update`, and `delete` services, controllers, and routes; enhance `getAllComments` to support filtering by `article_id` and `user_id`.

--- back/services/mongodb/comment.service.js
+++ back/services/mongodb/comment.service.js
@@ -1,20 +1,52 @@
 const { PrismaClient } = require('../../prisma/generated/mongodb');
 const prismaMongodb = new PrismaClient();
 
-async function getAllComments(){
-    return await prismaMongodb.comment.findMany();
+async function getAllComments(criterias = {}){
+    const where = {}
+    if (criterias.article_id) {
+        where.article_id = criterias.article_id;
+    }
+    if (criterias.id_user) {
+        where.id_user = criterias.id_user;
+    }
+    const comments = await prismaMongodb.comment.findMany({
+            where,
+        }
+    );
+    if(comments) {
+        return comments;
+    }
+    else {
+        return null;
+    }
 }
 
-async function getCommentsByArticleId(id){
-    console.log(id);
-    return prismaMongodb.comment.findMany({
-        where: {
-            article_id: id,
+async function deleteComment(id){
+    return prismaMongodb.comment.delete(
+        {
+            where: {
+                id: id,
+            }
+        }
+    )
+}
+
+async function createComment(comment){
+    //const article_id = comment.article_id;
+    return prismaMongodb.comment.create({data: comment});
+}
+
+async function updateComment(commentId, updatedData){
+    return prismaMongodb.comment.create({where: {
+            id: commentId
         },
-    })
+        update: updatedData}
+    );
 }
 
 module.exports = {
     getAllComments,
-    getCommentsByArticleId
+    deleteComment,
+    createComment,
+    updateComment,
 };
